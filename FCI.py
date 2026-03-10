f941ca523cb54827d13928af4fcf2553d72f79c2
simontassent <simon.tassent@ugent.be>
2026-03-10 11:30:20 +0100

    Construct matrix added

--- FCI.py
+++ FCI.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from Class_FCI import FCI
 
-Nx=10
-Ny=10
+Nx=3
+Ny=3
+Nt=10
+dx=np.ones(Nx)*2
+dy=np.ones(Ny)*3
+dt=4
+eps=np.ones(Nx*Ny)
+mu=np.ones(Nx*Ny)
+
+solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu)
+solver.construct_update_matrix()
