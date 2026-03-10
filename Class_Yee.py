3b66c55c4c295853efc1bda964971c8f8a8d3e23
IxsanderD <ixsanderdaems@gmail.com>
2026-03-10 12:25:33 +0100

    Recorder

--- Class_Yee.py
+++ Class_Yee.py
@@ -37,10 +37,16 @@ class Yee:
         self.width = width
         self.Wc = Wc
         
+    def add_recorder(self,xr,yr):
+        self.xr = xr
+        self.yr = yr
+        self.recorded_Ez = []
+        
     def restart(self):
         self.Ez = np.zeros((self.Nx+1,self.Ny+1))
         self.Hx = np.zeros((self.Nx+1,self.Ny))
         self.Hy = np.zeros((self.Nx,self.Ny+1))
+        self.recorded_Ez = []
         self.n = 0
     
     def update(self):
@@ -56,6 +62,7 @@ class Yee:
         #Update Hx:
         self.Hx[1:-1,:] = self.Hx[1:-1,:] - self.dt/(self.mux[1:-1,:]/self.dy[np.newaxis,:])*(self.Ez[1:-1,1:]-self.Ez[1:-1,:-1])
         self.n += 1
+        self.recorded_Ez.append(self.Ez[self.xr,self.yr])
     
     def update_loop(self,nt=None):
         if nt is None:
@@ -66,21 +73,30 @@ class Yee:
     def animate(self,speed=1,repeat=False):
         fig, ax = plt.subplots()
         im = ax.imshow(self.Ez.T,cmap='RdBu_r',extent=(0,self.L,0,self.L),vmin=-0.1,vmax=0.1)
-        ax.set_xlabel('x [m]')
-        ax.set_ylabel('y [m]')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
         ax.set_xlim(0,self.L)
         ax.set_ylim(0,self.L)
         ax.set_title('Ez')
         source_marker, = ax.plot(sum(self.dx[:self.xs]), sum(self.dy[:self.ys]), 'o', color='black', label='source', markersize=2, zorder=3)
-        # rec1, = ax.plot(self.x_recorders[0], self.y_recorders[0], 'x', color='red', label='recorder 1', zorder=3, markersize=6)
+        rec1, = ax.plot(sum(self.dx[:self.xr]), sum(self.dy[:self.yr]), 'x', color='red', label='recorder 1', zorder=3, markersize=6)
         def update(frame):
             self.update_loop(speed)
             im.set_data(self.Ez.T)
-            return [im]
+            return [im,source_marker,rec1]
         
         ani = FuncAnimation(fig, update, frames=self.Nt//speed, interval=int(self.dt * 1000), repeat=repeat)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="3%", pad=0.05)
         cb = fig.colorbar(im, cax=cax, label='Ez [V/m]')
-        ax.legend(loc='upper left') # For source and recorders
+        ax.legend(loc='upper left')
+        plt.show()
+        
+    def show_recorder(self):
+        plt.figure()
+        plt.plot(np.arange(self.n)*self.dt, self.recorded_Ez)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Ez at recorder [V/m]')
+        plt.title(f'Recorded Ez at ({round(sum(self.dx[:self.xr]),1)}, {round(sum(self.dy[:self.yr]),1)})')
+        plt.grid()
         plt.show()
--- Yee.py
+++ Yee.py
@@ -8,15 +8,27 @@ Ny = 200
 c = 1
 CFL = 1
 dt = CFL/c/np.sqrt(1/np.min(L/Nx)**2+1/np.min(L/Ny)**2)
-xs = Nx//4
-ys = Ny//2
 J0 = 10
 width = L/c
 tc = 5*width
 Wc = 2*np.pi*0.5/tc
-Nt = int(10 * tc / dt)
+Nt = int(7*tc/dt)
+
+xs = Nx//4
+ys = Ny//2
+xr = Nx//2
+yr = Ny//2
+
+###
+# Without PML
+###
 
 solver = Yee(L,Nx,Ny,Nt,dt)
 solver.add_source(xs,ys,J0,tc,width,Wc)
-solver.animate(speed = 20)
+solver.add_recorder(xr,yr)
+solver.animate(speed = 60)
+solver.restart()
+
+solver.update_loop()
+solver.show_recorder()
 solver.restart()
