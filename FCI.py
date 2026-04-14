242f8e7962662e09ac4fe333499827a1553122cf
jarnelonneux <jarne.lonneux@gmail.com>
2026-04-14 19:29:54 +0200

    Sinusbron toegevoegd aan FCI + spelen met parameters

--- FCI.py
+++ FCI.py
@@ -8,21 +8,30 @@ from scipy.special import hankel2
 
 start=time.perf_counter()
 
+L = 1
 Nx=51
 Ny=51
-Nt=100
-dx=np.ones(Nx)
-dy=np.ones(Ny)
+Nt=500
+dx=np.ones(Nx)*L/Nx
+dy=np.ones(Ny)*L/Ny
 c0=1
 eps=1
 mu=1
 J0 = 10
-width = np.sum(dx)/(10*c0)
+# width = np.sum(dx)/(10*c0)
+# tc = 5*width
+# tf= 5*tc
+# dt=tf/Nt
+# Wc = 5/width
+Wc = 0.05*2*np.pi
+width = 5/Wc
 tc = 5*width
-tf= 5*tc
-dt=tf/Nt
+dt = 5*tc/Nt
+
+m = 4
 k_max=1
-sigma_max=1
+sigma_max=10
+# (m+1)/(150*np.pi*dx[0])
 
 # xs = Nx//2
 # ys = Ny//2
@@ -32,7 +41,7 @@ sigma_max=1
 # solver.construct_update_matrix()
 # end=time.perf_counter()
 # print(f"Runtime: {end - start:.6f} seconds")
-# solver.add_source(xs,ys,J0,tc,width)
+# solver.add_source(xs,ys,J0,tc,width,Wc)
 # solver.add_recorder(xs,ys)
 # # solver.update_loop()
 # # solver.show_recorder()
@@ -65,15 +74,19 @@ xr = Nx//4
 yr = Ny//4
 
 solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
-solver.construct_update_matrix()
+# solver.construct_update_matrix()
 end=time.perf_counter()
 print(f"Runtime: {end - start:.6f} seconds")
-solver.add_source(xs,ys,J0,tc,width)
-solver.add_recorder(xr,yr)
-solver.animate()
+solver.add_source(xs,ys,J0,tc,width,Wc)
+plt.plot(np.arange(solver.Nt)*solver.dt, solver.applied_source, label='Applied source')
+plt.show()
+plt.plot(np.fft.rfftfreq(solver.Nt, solver.dt), np.abs(np.fft.rfft(solver.applied_source)*dt), label='Applied source (frequency domain)')
+plt.show()
+# solver.add_recorder(xr,yr)
+# solver.animate()
 
-solver.restart()
-solver.update_loop()
+# solver.restart()
+# solver.update_loop()
 # solver.show_recorder()
 
 # # Plot time domain response
@@ -85,32 +98,32 @@ solver.update_loop()
 # plt.title('Time domain response')
 # plt.show()
 
-E_freq_sim = np.fft.rfft(solver.recorded_Ez)*dt
-source_freq = np.fft.rfft(solver.applied_source)*dt
-omega = 2*np.pi*np.fft.rfftfreq(len(solver.recorded_Ez), dt)
-E_freq_ana = -J0*omega/4*hankel2(0, omega/c*np.sqrt((xr-xs)**2+(yr-ys)**2))
-E_freq_ana[0] = 0
+# E_freq_sim = np.fft.rfft(solver.recorded_Ez)*dt
+# source_freq = np.fft.rfft(solver.applied_source)*dt
+# omega = 2*np.pi*np.fft.rfftfreq(len(solver.recorded_Ez), dt)
+# E_freq_ana = -J0*omega/4*hankel2(0, omega/c*np.sqrt((xr-xs)**2+(yr-ys)**2))
+# E_freq_ana[0] = 0
 
-# Restrict to bandwidth of the source
-E_max = np.max(np.abs(source_freq))
-mask = (np.abs(source_freq) > 0.005*E_max)
+# # Restrict to bandwidth of the source
+# E_max = np.max(np.abs(source_freq))
+# mask = (np.abs(source_freq) > 0.005*E_max)
 
-# Rescale
-E_freq_sim *= np.mean(np.abs(E_freq_ana[mask]/J0/E_freq_sim[mask]*source_freq[mask]))
+# # Rescale
+# E_freq_sim *= np.mean(np.abs(E_freq_ana[mask]/J0/E_freq_sim[mask]*source_freq[mask]))
 
-# Plot frequency domain response
-plt.plot(omega, np.abs(E_freq_sim), label='Electric field at recorder (V/m)')
-plt.plot(omega, np.abs(source_freq), label='Applied source current density (A/m^2)')
-plt.xlabel('Frequency (rad/s)')
-plt.legend()
-plt.title('Frequency domain response')
-plt.show()
+# # Plot frequency domain response
+# plt.plot(omega, np.abs(E_freq_sim), label='Electric field at recorder (V/m)')
+# plt.plot(omega, np.abs(source_freq), label='Applied source current density (A/m^2)')
+# plt.xlabel('Frequency (rad/s)')
+# plt.legend()
+# plt.title('Frequency domain response')
+# plt.show()
 
-# Compare with analytical solution
-plt.plot(omega[mask], np.abs(E_freq_sim/source_freq)[mask], label='Numerical response (rescaled)')
-plt.plot(omega[mask], np.abs(E_freq_ana/J0)[mask], label='Analytical response')
-plt.xlabel('Frequency (rad/s)')
-plt.ylabel('|E_z/J|')
-plt.legend()
-plt.title('Frequency response comparison')
-plt.show()
+# # Compare with analytical solution
+# plt.plot(omega[mask], np.abs(E_freq_sim/source_freq)[mask], label='Numerical response (rescaled)')
+# plt.plot(omega[mask], np.abs(E_freq_ana/J0)[mask], label='Analytical response')
+# plt.xlabel('Frequency (rad/s)')
+# plt.ylabel('|E_z/J|')
+# plt.legend()
+# plt.title('Frequency response comparison')
+# plt.show()
