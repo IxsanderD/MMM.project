FCI plot fixed + analytical in progress + Yee analytical better scaling

Class_FCI.py
@@ -28,7 +28,7 @@ class FCI:
 
     def add_source(self,xs,ys,J0,tc,width):
         self.xs = xs
-        self.ys = ys
+        self.ys = self.Ny - ys
         self.J0 = J0
         self.tc = tc
         self.width = width
@@ -37,7 +37,7 @@ class FCI:
 
     def add_recorder(self,xr,yr):
         self.xr = xr
-        self.yr = yr
+        self.yr = self.Ny - yr
         self.recorded_Ez = []
 
     def add_material(self,x_start,x_end,y_start,y_end,eps_r,mu_r,sigma):
@@ -180,14 +180,16 @@ class FCI:
         ax.set_xlim(0,np.sum(self.dx))
         ax.set_ylim(0,np.sum(self.dy))
         ax.set_title('Ez')
-        source_marker, = ax.plot(sum(self.dx[:self.xs]), sum(self.dy[:self.ys]), 'o', color='black', label='source', markersize=2, zorder=3)
+        source_marker, = ax.plot(sum(self.dx[:self.xs+1]), self.Ny - sum(self.dy[:self.ys+1]), 'o', color='black', label='source', markersize=2, zorder=3)
+        rec1, = ax.plot(sum(self.dx[:self.xr+1]), self.Ny - sum(self.dy[:self.yr+1]), 'x', color='red', label='recorder 1', zorder=3, markersize=6)
         def update(frame):
             self.update_loop(speed)
             Ez=np.reshape(self.all_fields[:self.Nx*self.Ny],(self.Nx,self.Ny))
             im.set_data(Ez.T)
-            return [im,source_marker]
+            ax.set_title('Ez at t = {:.2f} s'.format(self.n*self.dt))
+            return [im,source_marker, rec1]
         
-        ani = FuncAnimation(fig, update, frames=self.Nt//speed, interval=int(self.dt), repeat=repeat, blit=True)
+        ani = FuncAnimation(fig, update, frames=self.Nt//speed, interval=int(self.dt), repeat=repeat)
         # ani.save("simulation.gif", writer="pillow", fps=10)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="3%", pad=0.05)

FCI.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.constants import c, epsilon_0, mu_0
 from Class_FCI import FCI
 import time
+from scipy.special import hankel2
 
 start=time.perf_counter()
 
@@ -22,21 +23,21 @@ dt=tf/Nt
 k_max=1
 sigma_max=1
 
-xs = Nx//2
-ys = Ny//2
+# xs = Nx//2
+# ys = Ny//2
 
-solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
-solver.add_material(3*Nx//5,4*Nx//5,3*Ny//5,4*Ny//5,2,2,1)
-solver.construct_update_matrix()
-end=time.perf_counter()
-print(f"Runtime: {end - start:.6f} seconds")
-solver.add_source(xs,ys,J0,tc,width)
-solver.add_recorder(xs,ys)
-# solver.update_loop()
-# solver.show_recorder()
-solver.animate()
+# solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
+# solver.add_material(3*Nx//5,4*Nx//5,3*Ny//5,4*Ny//5,2,2,1)
+# solver.construct_update_matrix()
+# end=time.perf_counter()
+# print(f"Runtime: {end - start:.6f} seconds")
+# solver.add_source(xs,ys,J0,tc,width)
+# solver.add_recorder(xs,ys)
+# # solver.update_loop()
+# # solver.show_recorder()
+# solver.animate()
 
-import plotly.express as px
+# import plotly.express as px
 
 # Example matrix (replace with your own)
 # matrix = solver.left_matrix.toarray()
@@ -52,3 +53,63 @@ import plotly.express as px
 # )
 
 # fig.show()
+
+###
+# Analytical verification
+###
+
+xs = Nx//4
+ys = 3*Ny//4
+xr = Nx//4
+yr = Ny//4
+
+solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
+solver.construct_update_matrix()
+end=time.perf_counter()
+print(f"Runtime: {end - start:.6f} seconds")
+solver.add_source(xs,ys,J0,tc,width)
+solver.add_recorder(xr,yr)
+solver.animate()
+
+solver.restart()
+solver.update_loop()
+# solver.show_recorder()
+
+# # Plot time domain response
+# plt.plot(solver.recorded_Ez, label='Simulated recorder')
+# plt.plot(solver.applied_source, label='Applied source')
+# plt.xlabel('Time (s)')
+# plt.ylabel('|E_z|')
+# plt.legend()
+# plt.title('Time domain response')
+# plt.show()
+
+E_freq_sim = np.fft.rfft(solver.recorded_Ez)*dt
+source_freq = np.fft.rfft(solver.applied_source)*dt
+omega = 2*np.pi*np.fft.rfftfreq(len(solver.recorded_Ez), dt)
+E_freq_ana = -J0*omega/4*hankel2(0, omega/c*np.sqrt((xr-xs)**2+(yr-ys)**2))
+E_freq_ana[0] = 0
+
+# Restrict to bandwidth of the source
+E_max = np.max(np.abs(source_freq))
+mask = (np.abs(source_freq) > 0.005*E_max)
+
+# Rescale
+E_freq_sim *= np.mean(np.abs(E_freq_ana[mask]/J0/E_freq_sim[mask]*source_freq[mask]))
+
+# Plot frequency domain response
+plt.plot(omega, np.abs(E_freq_sim), label='Electric field at recorder (V/m)')
+plt.plot(omega, np.abs(source_freq), label='Applied source current density (A/m^2)')
+plt.xlabel('Frequency (rad/s)')
+plt.legend()
+plt.title('Frequency domain response')
+plt.show()
+
+# Compare with analytical solution
+plt.plot(omega[mask], np.abs(E_freq_sim/source_freq)[mask], label='Numerical response (rescaled)')
+plt.plot(omega[mask], np.abs(E_freq_ana/J0)[mask], label='Analytical response')
+plt.xlabel('Frequency (rad/s)')
+plt.ylabel('|E_z/J|')
+plt.legend()
+plt.title('Frequency response comparison')
+plt.show()

Yee.py
@@ -62,7 +62,7 @@ solver.add_source(xs,ys,J0,tc,width,Wc)
 solver.add_recorder(xr,yr)
 solver.update_loop()
 
-# Plot time domain response
+# # Plot time domain response
 # plt.plot(solver.recorded_Ez, label='Simulated recorder')
 # plt.plot(solver.applied_source, label='Applied source')
 # plt.xlabel('Time (s)')
@@ -76,14 +76,14 @@ source_freq = np.fft.rfft(solver.applied_source)*dt
 omega = 2*np.pi*np.fft.rfftfreq(len(solver.recorded_Ez), dt)
 
 E_freq_ana = -J0*omega/4*hankel2(0, omega/c*np.sqrt((xr-xs)**2+(yr-ys)**2))
+E_freq_ana[0] = 0
 
 # Restrict to bandwidth of the source
 E_max = np.max(np.abs(source_freq))
-mask = np.abs(source_freq) > 0.005*E_max
+mask = (np.abs(source_freq) > 0.005*E_max)
 
 # Rescale
-index = len(omega[mask])//2
-E_freq_sim *= np.abs(E_freq_ana[index]/J0)/np.abs(E_freq_sim[index]/source_freq[index])
+E_freq_sim *= np.mean(np.abs(E_freq_ana[mask]/J0/E_freq_sim[mask]*source_freq[mask]))
 
 # Plot frequency domain response
 plt.plot(omega[mask], np.abs(E_freq_sim)[mask], label='Simulated recorder')