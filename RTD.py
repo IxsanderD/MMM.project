3dd1a6af763ba7805db22446d895cb5409d0eaea
IxsanderD <ixsanderdaems@gmail.com>
2026-04-14 19:56:19 +0200

    HELP

--- RTD.py
+++ RTD.py
@@ -14,23 +14,22 @@ Ly = 40
 Lz = Ly
 dx = Lx/3000
 U0 = 0.6*e.value*10**(-18)
-dt = 0.7*2/(2*hbar.value/(0.023*m_e.value*dx**2)+U0/hbar.value)
-x0 = a/3
+dt = 0.8*2/(2*hbar.value/(0.023*m_e.value*dx**2)+U0/hbar.value)
+x0 = a/2
 sigma_x = a/10
 xr = 7*a/3+2*b
 
-m = 1
-n = 1
+m = 20
+n = 20
 
 m_eff = 0.023*m_e.value
-E = hbar.value**2/(2*m_eff)*((np.pi*n/Ly)**2+(np.pi*m/Lz)**2)
-print(f'Energy: {E/e.value*10**18:.2f} eV')
-kx = np.sqrt(2*m_eff*E/hbar.value**2) # in 1/nm
-sigma = 5*E
-k = 3 # exponent for the absorbing boundary strength
+E = hbar.value**2/(2*m_eff)*((np.pi*n/Ly)**2+(np.pi*m/Lz)**2)*10**18 # in J
+print(f'Energy: {E/e.value} eV')
+kx = np.sqrt(2*m_eff*E/hbar.value**2)*10**(-9) # in 1/nm
+sigma = 5*E*10**(-18) # maybe sqrt(E)? idk
+k = 4 # exponent for the absorbing boundary strength
 N_layer = 200
-t_max = 20000/kx
-speed = int(t_max*Lx/500)
+t_max = 8000
 
 ###
 # Without Absorbing Boundaries:
@@ -38,7 +37,7 @@ speed = int(t_max*Lx/500)
 
 # solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=False)
 # solver.add_recorder(xr)
-# solver.animate(speed = speed)
+# solver.animate(speed = 1000)
 
 ###
 # With Absorbing Boundaries:
@@ -46,17 +45,17 @@ speed = int(t_max*Lx/500)
 
 # solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
 # solver.add_recorder(xr)
-# solver.animate(speed = speed)
+# solver.animate(speed = 1000)
 
 ###
 # With potential barriers:
 ###
 
-# solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
+solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
 
-# solver.add_barriers(U0)
-# solver.add_recorder(xr)
-# solver.animate(speed = speed)
+solver.add_barriers(U0)
+solver.add_recorder(xr)
+solver.animate(speed = 1000)
 
 # solver.restart()
 # solver.update_loop_2()
@@ -80,13 +79,30 @@ speed = int(t_max*Lx/500)
 # Validation with analytical solution:
 ###
 
-solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
-solver.add_barriers(U0)
-solver.add_recorder(xr)
+# solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
+# solver.add_barriers(U0)
+# solver.add_recorder(xr)
 
-E,T_ana = solver.analytical_T()
-plt.plot(np.real(E)/e.value*10**18,T_ana,label='Analytical')
-plt.xlabel('Energy [eV]')
-plt.ylabel('Transmission')
-plt.legend()
-plt.show()
+# E_ana,T_ana = solver.analytical_T()
+
+# solver.update_loop_2()
+# solver.show_recorder()
+# t, J_time = solver.J_time()
+# E, J_barrier = solver.J_freq(t,J_time)
+
+# solver = RTD(dx,dt,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,ABC=True)
+# solver.add_recorder(xr)
+
+# solver.update_loop_2()
+# solver.show_recorder()
+# t, J_time = solver.J_time()
+# E_num, J_free = solver.J_freq(t,J_time)
+
+# T_num = np.abs(J_barrier/J_free)
+# plt.plot(E_num/e.value*10**18,T_num,label='Numerical')
+# plt.plot(np.real(E_ana)/e.value*10**18,T_ana,label='Analytical')
+# plt.xlabel('Energy [eV]')
+# plt.ylabel('Transmission')
+# plt.xlim(0,0.6)
+# plt.legend()
+# plt.show()
