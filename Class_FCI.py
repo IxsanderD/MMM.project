b6eba6095ee6a4c2a1f9f898142f5db34b09c9d3
simontassent <simon.tassent@ugent.be>
2026-04-15 19:38:32 +0200

    Drude FCI

--- Class_FCI.py
+++ Class_FCI.py
@@ -8,7 +8,7 @@ import time
 from scipy.special import hankel2
 
 class FCI:
-    def __init__(self,Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max):
+    def __init__(self,Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max,drude=False):
         self.Nx=Nx
         self.Ny=Ny
         self.Nt=Nt
@@ -17,12 +17,17 @@ class FCI:
         self.dx_dual = np.append((self.dx[1:]+self.dx[:-1])/2,(self.dx[0]+self.dx[-1])/2)
         self.dy_dual = np.append((self.dy[1:]+self.dy[:-1])/2,(self.dy[0]+self.dy[-1])/2)
         self.dt=dt
-        self.all_fields=np.zeros(6*Nx*Ny) # Voor ordening, kijk pagina 14 van de cursus
+        self.drude=drude
+        if drude:
+            self.all_fields=np.zeros(8*Nx*Ny)
+        else:
+            self.all_fields=np.zeros(6*Nx*Ny) # Voor ordening, kijk pagina 14 van de cursus
         self.source_index=[]
         self.eps=eps*np.ones(self.Nx*self.Ny) # Zelfde ordening als velden
         self.mu=mu*np.ones(self.Nx*self.Ny) # Zelfde ordening als velden
         self.c = 1/np.sqrt(np.min(eps)*np.min(mu))
         self.sigma=np.zeros(self.Nx*self.Ny)
+        self.gamma=np.zeros(self.Nx*self.Ny)
         self.n=0
         self.kmax=k_max
         self.sigmamax=sigma_max
@@ -43,7 +48,7 @@ class FCI:
         self.yr = self.Ny - yr
         self.recorded_Ez = []
 
-    def add_material(self,x_start,x_end,y_start,y_end,eps_r,mu_r,sigma):
+    def add_material(self,x_start,x_end,y_start,y_end,eps_r,mu_r,sigma,gamma=0):
         eps=np.reshape(self.eps,(self.Nx,self.Ny))
         eps[x_start:x_end,y_start:y_end]=eps_r*self.eps[0]
         self.eps=np.ravel(eps)
@@ -55,6 +60,10 @@ class FCI:
         sig=np.reshape(self.sigma,(self.Nx,self.Ny))
         sig[x_start:x_end,y_start:y_end]=sigma
         self.sigma=np.ravel(sig)
+
+        gam=np.reshape(self.gamma,(self.Nx,self.Ny))
+        gam[x_start:x_end,y_start:y_end]=gamma
+        self.gamma=np.ravel(gam)
     
     def construct_update_matrix(self):
         Dx=-diags(np.ones(self.Nx),offsets=0,format='csr')+diags(np.ones(self.Nx-1),offsets=1,format='csr')+diags(np.ones(1),offsets=-self.Nx+1,format='csr')
@@ -166,6 +175,136 @@ class FCI:
         S=bmat([[A00,A01,Z],[A14@A44_inv@A45@A55_inv@A50,A11,A12],[-A23@A33_inv@A30,Z,A22]],format='csc')
         self.S_LU=splu(S)
 
+    def construct_update_matrix_drude(self):
+        Dx=-diags(np.ones(self.Nx),offsets=0,format='csr')+diags(np.ones(self.Nx-1),offsets=1,format='csr')+diags(np.ones(1),offsets=-self.Nx+1,format='csr')
+        Dy=-diags(np.ones(self.Ny),offsets=0,format='csr')+diags(np.ones(self.Ny-1),offsets=1,format='csr')+diags(np.ones(1),offsets=-self.Ny+1,format='csr')
+        Ax=1/2*(diags(np.ones(self.Nx),offsets=0,format='csr')+diags(np.ones(self.Nx-1),offsets=1,format='csr')+diags(np.ones(1),offsets=-self.Nx+1,format='csr'))
+        Ay=1/2*(diags(np.ones(self.Ny),offsets=0,format='csr')+diags(np.ones(self.Ny-1),offsets=1,format='csr')+diags(np.ones(1),offsets=-self.Ny+1,format='csr'))
+        delta_x_inv=diags(1/self.dx,offsets=0,format='csr')
+        delta_y_inv=diags(1/self.dy,offsets=0,format='csr')
+        k_array=np.array([1+(self.kmax-1)*(i/10)**4 for i in range(1,11)])
+        K_y=np.ones((self.Nx,self.Ny))
+        K_y[:,self.Ny-10:]=np.tile(k_array,(self.Ny,1))
+        K_y[:,:10]=np.tile(np.flip(k_array),(self.Ny,1))
+        K_x=np.ones((self.Nx,self.Ny))
+        k_array_v=np.reshape(k_array,(10,1))
+        K_x[self.Nx-10:,:]=np.repeat(k_array_v,self.Nx,axis=1)
+        k_array_v=np.reshape(np.flip(k_array),(10,1))
+        K_x[:10,:]=np.repeat(k_array_v,self.Nx,axis=1)
+        sigma_array=np.array([self.sigmamax*(i/10)**4 for i in range(1,11)]) # 5/150/np.pi/np.mean(self.dx)
+        sigma_y=np.zeros((self.Nx,self.Ny))
+        sigma_y[:,self.Ny-10:]=np.tile(sigma_array,(self.Ny,1))
+        sigma_y[:,:10]=np.tile(np.flip(sigma_array),(self.Ny,1))
+        sigma_array_v=np.reshape(sigma_array,(10,1))
+        sigma_x=np.zeros((self.Nx,self.Ny))
+        sigma_x[self.Nx-10:,:]=np.repeat(sigma_array_v,self.Nx,axis=1)
+        sigma_array_v=np.reshape(np.flip(sigma_array),(10,1))
+        sigma_x[:10,:]=np.repeat(sigma_array_v,self.Nx,axis=1)
+
+        K_y=np.ravel(K_y)
+        K_y_dt=diags(K_y,offsets=0,format='csr')/self.dt
+        K_x=np.ravel(K_x)
+        K_x_dt=diags(K_x,offsets=0,format='csr')/self.dt
+        sigma_x=np.ravel(sigma_x)
+        sigma_x_2=diags(sigma_x,offsets=0,format='csr')/2
+        sigma_y=np.ravel(sigma_y)
+        sigma_y_2=diags(sigma_y,offsets=0,format='csr')/2
+        eps=diags(self.eps,offsets=0,format='csr')
+        eps_inv=diags(1/self.eps,offsets=0,format='csr')
+        mu=diags(self.mu,offsets=0,format='csr')
+        sigma=diags(self.sigma,offsets=0,format='csr')
+        gamma=diags(self.gamma,offsets=0,format='csr')
+
+        A_x=kron(np.eye(self.Nx),Ay,format='csr')
+        A_y=kron(Ax,np.eye(self.Ny),format='csr')
+        A_z=kron(Ax,Ay,format='csr')
+
+        # block (0,0)
+        A00=-A_z@(K_y_dt+sigma_y_2@eps_inv)
+        B00=-A_z@(K_y_dt-sigma_y_2@eps_inv)
+
+        # block (0,1)
+        A01=A_z/self.dt
+        B01=A01
+
+        # block (1,2)
+        A12=kron(Ax,delta_y_inv@Dy,format='csr')/2
+        B12=-kron(Ax,delta_y_inv@Dy,format='csr')/2
+
+        # block (1,4)
+        A14=-kron(delta_x_inv@Dx,Ay,format='csr')/2
+        B14=kron(delta_x_inv@Dx,Ay,format='csr')/2
+
+        # block (1,6)
+        A16=A_z@eps/self.dt
+        B16=A_z@eps/self.dt
+
+        # block (1,7)
+        A17=A_z/2
+        B17=-A_z/2
+
+        # block (2,2)
+        A22=-A_x/self.dt
+        B22=-A_x/self.dt
+
+        # block (2,3)
+        A23=A_x@(K_x_dt+sigma_x_2@eps_inv)
+        B23=A_x@(K_x_dt-sigma_x_2@eps_inv)
+
+        # block (3,0)
+        A30=kron(eye(self.Nx,format='csr'),delta_y_inv@Dy,format='csr')/2
+        B30=-kron(eye(self.Nx,format='csr'),delta_y_inv@Dy,format='csr')/2
+
+        # block (3,3)
+        A33=A_x@(mu@K_y_dt+mu@sigma_y_2@eps_inv)
+        B33=A_x@(mu@K_y_dt-mu@sigma_y_2@eps_inv)
+
+        # block (4,4)
+        A44=-A_y@(K_x_dt+sigma_x_2@eps_inv)
+        B44=-A_y@(K_x_dt-sigma_x_2@eps_inv)
+
+        # block (4,5)
+        A45=A_y@(K_y_dt+sigma_y_2@eps_inv)
+        B45=A_y@(K_y_dt-sigma_y_2@eps_inv)
+
+        # block (5,0)
+        A50=-kron(delta_x_inv@Dx,eye(self.Ny,format='csr'),format='csr')/2
+        B50=kron(delta_x_inv@Dx,eye(self.Ny,format='csr'),format='csr')/2
+
+        # block (5,5)
+        A55=A_y@mu/self.dt
+        B55=A_y@mu/self.dt
+
+        # block (6,1)
+        A61=-A_z@(K_x_dt+sigma_x_2@eps_inv)
+        B61=-A_z@(K_x_dt-sigma_x_2@eps_inv)
+
+        # block (6,6)
+        A66=A_z/self.dt
+        B66=A_z/self.dt
+
+        # Block (7,6)
+        A76=A_z@sigma/2
+        B76=-A_z@sigma/2
+
+        # Block (7,7)
+        A77=-A_z@(gamma/self.dt)-A_z/2
+        B77=-A_z@(gamma/self.dt)+A_z/2
+
+        Z=csr_array((self.Nx*self.Ny, self.Nx*self.Ny))
+
+        self.M11=bmat([[A00,A01,Z,Z,Z],[Z,Z,A12,Z,A14],[Z,Z,A22,A23,Z],[A30,Z,Z,A33,Z],[Z,Z,Z,Z,A44]],format='csr')
+        self.M12=bmat([[Z,Z,Z],[Z,A16,A17],[Z,Z,Z],[Z,Z,Z],[A45,Z,Z]],format='csr')
+        self.M21=bmat([[A50,Z,Z,Z,Z],[Z,A61,Z,Z,Z],[Z,Z,Z,Z,Z]],format='csr')
+        self.M22=bmat([[A55,Z,Z],[Z,A66,Z],[Z,A76,A77]],format='csc')
+        self.right_matrix=bmat([[B00,B01,Z,Z,Z,Z,Z,Z],[Z,Z,B12,Z,B14,Z,B16,B17],[Z,Z,B22,B23,Z,Z,Z,Z],[B30,Z,Z,B33,Z,Z,Z,Z],[Z,Z,Z,Z,B44,B45,Z,Z],[B50,Z,Z,Z,Z,B55,Z,Z],[Z,B61,Z,Z,Z,Z,B66,Z],[Z,Z,Z,Z,Z,Z,B76,B77]],format='csr')
+        A55_inv=inv(A55.tocsc()).tocsr()
+        A66_inv=inv(A66.tocsc()).tocsr()
+        A77_inv=inv(A77.tocsc()).tocsr()
+        self.M22_LU=splu(self.M22)
+        S=bmat([[A00,A01,Z,Z,Z],[Z,-A16@A66_inv@A61+A17@A77_inv@A76@A66_inv@A61,A12,Z,A14],[Z,Z,A22,A23,Z],[A30,Z,Z,A33,Z],[-A45@A55_inv@A50,Z,Z,Z,A44]],format='csc')
+        self.S_LU=splu(S)
+
     def restart(self):
         self.all_fields=np.zeros(6*self.Nx*self.Ny)
         self.n=0
@@ -173,12 +312,24 @@ class FCI:
     
     def update(self):
         b1=self.right_matrix[:3*self.Nx*self.Ny,:]@self.all_fields
-        # b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
-        b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.sin(self.Wc*self.n*self.dt)*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
+        b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
+        # b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.sin(self.Wc*self.n*self.dt)*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
         b2=self.right_matrix[3*self.Nx*self.Ny:,:]@self.all_fields
         M22_inv_b2=self.M22_LU.solve(b2)
         self.all_fields[:3*self.Nx*self.Ny]=self.S_LU.solve(b1-self.M12@M22_inv_b2)
         self.all_fields[3*self.Nx*self.Ny:]=self.M22_LU.solve(b2-self.M21@self.all_fields[:3*self.Nx*self.Ny])
+        self.n+=1
+        Ez=np.reshape(self.all_fields[:self.Nx*self.Ny],(self.Nx,self.Ny))
+        self.recorded_Ez.append(Ez[self.xr,self.yr])
+    
+    def update_drude(self):
+        b1=self.right_matrix[:5*self.Nx*self.Ny,:]@self.all_fields
+        b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
+        # b1[self.Nx*self.Ny+self.xs*self.Ny+self.ys]+=self.J0*np.sin(self.Wc*self.n*self.dt)*np.exp(-(self.n*self.dt-self.tc)**2/(2*self.width**2))
+        b2=self.right_matrix[5*self.Nx*self.Ny:,:]@self.all_fields
+        M22_inv_b2=self.M22_LU.solve(b2)
+        self.all_fields[:5*self.Nx*self.Ny]=self.S_LU.solve(b1-self.M12@M22_inv_b2)
+        self.all_fields[5*self.Nx*self.Ny:]=self.M22_LU.solve(b2-self.M21@self.all_fields[:5*self.Nx*self.Ny])
         self.n+=1
         Ez=np.reshape(self.all_fields[:self.Nx*self.Ny],(self.Nx,self.Ny))
         self.recorded_Ez.append(Ez[self.xr,self.yr])
@@ -188,6 +339,12 @@ class FCI:
             nt = self.Nt
         for _ in range(nt):
             self.update()
+    
+    def update_loop_drude(self,nt=None):
+        if nt is None:
+            nt = self.Nt
+        for _ in range(nt):
+            self.update_drude()
 
     def animate(self,speed=1,repeat=False):
         Ez=np.reshape(self.all_fields[:self.Nx*self.Ny],(self.Nx,self.Ny))
@@ -201,7 +358,10 @@ class FCI:
         source_marker, = ax.plot(sum(self.dx[:self.xs+1]), np.sum(self.dy) - sum(self.dy[:self.ys+1]), 'o', color='black', label='source', markersize=2, zorder=3)
         rec1, = ax.plot(sum(self.dx[:self.xr+1]), np.sum(self.dy) - sum(self.dy[:self.yr+1]), 'x', color='red', label='recorder 1', zorder=3, markersize=6)
         def update(frame):
-            self.update_loop(speed)
+            if self.drude:
+                self.update_loop_drude(speed)
+            else:
+                self.update_loop(speed)
             Ez=np.reshape(self.all_fields[:self.Nx*self.Ny],(self.Nx,self.Ny))
             im.set_data(Ez.T)
             ax.set_title('Ez at t = {:.2f} s'.format(self.n*self.dt))
--- FCI.py
+++ FCI.py
@@ -11,40 +11,44 @@ start=time.perf_counter()
 L = 1
 Nx=51
 Ny=51
-Nt=500
+Nt=100
 dx=np.ones(Nx)*L/Nx
 dy=np.ones(Ny)*L/Ny
 c0=1
 eps=1
 mu=1
-J0 = 10
-# width = np.sum(dx)/(10*c0)
-# tc = 5*width
-# tf= 5*tc
-# dt=tf/Nt
-# Wc = 5/width
-Wc = 2*np.pi*c0/(7*dx[0])
-width = 5/Wc
+J0 = 20
+width = np.sum(dx)/(10*c0)
 tc = 5*width
-dt = 5*tc/Nt
+tf= 5*tc
+dt=tf/Nt
+Wc = 5/width
+# Wc = 2*np.pi*c0/(7*dx[0])
+# width = 5/Wc
+# tc = 5*width
+# dt = 5*tc/Nt
 
-m = 4
+# m = 4
+# k_max=1
+# sigma_max= 100 #(m+1)/(150*np.pi*dx[0])
+
+m=4
 k_max=1
-sigma_max= 100 #(m+1)/(150*np.pi*dx[0])
+sigma_max=100 #(m+1)/(150*np.pi*dx[0])
 
-# xs = Nx//2
-# ys = Ny//2
+xs = Nx//2
+ys = Ny//2
 
-# solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
-# solver.add_material(3*Nx//5,4*Nx//5,3*Ny//5,4*Ny//5,2,2,1)
-# solver.construct_update_matrix()
-# end=time.perf_counter()
-# print(f"Runtime: {end - start:.6f} seconds")
-# solver.add_source(xs,ys,J0,tc,width,Wc)
-# solver.add_recorder(xs,ys)
+solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max,drude=True)
+solver.add_material(3*Nx//5,4*Nx//5,3*Ny//5,4*Ny//5,1,1,1000,gamma=3)
+solver.construct_update_matrix_drude()
+solver.add_source(xs,ys,J0,tc,width,Wc)
+solver.add_recorder(xs,ys)
 # # solver.update_loop()
 # # solver.show_recorder()
-# solver.animate()
+solver.animate()
+end=time.perf_counter()
+print(f"Runtime: {end - start:.6f} seconds")
 
 # import plotly.express as px
 
@@ -67,17 +71,17 @@ sigma_max= 100 #(m+1)/(150*np.pi*dx[0])
 # Analytical verification
 ###
 
-xs = Nx//4
-ys = 3*Ny//4
-xr = Nx//4
-yr = Ny//4
+# xs = Nx//4
+# ys = 3*Ny//4
+# xr = Nx//4
+# yr = Ny//4
 
-solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
-solver.construct_update_matrix()
-end=time.perf_counter()
-print(f"Runtime: {end - start:.6f} seconds")
-solver.add_source(xs,ys,J0,tc,width,Wc)
-solver.add_recorder(xr,yr)
+# solver=FCI(Nx,Ny,Nt,dx,dy,dt,eps,mu,k_max,sigma_max)
+# solver.construct_update_matrix()
+# end=time.perf_counter()
+# print(f"Runtime: {end - start:.6f} seconds")
+# solver.add_source(xs,ys,J0,tc,width,Wc)
+# solver.add_recorder(xr,yr)
 
-solver.update_loop()
-solver.analytical_solution(plot_all=True, frequency_limit = None)
+# solver.update_loop()
+# solver.analytical_solution(plot_all=True, frequency_limit = None)
