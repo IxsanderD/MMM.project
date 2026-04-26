b59ea52e6d37502485364c21817b021b5e13a953
IxsanderD <ixsanderdaems@gmail.com>
2026-04-26 10:50:35 +0200

    mask

--- Comparison_ana.py
+++ Comparison_ana.py
@@ -12,7 +12,7 @@ Ny = 351
 L = 1
 CFL = 0.90
 dt = CFL/c/np.sqrt(1/(L/Nx)**2+1/(L/Ny)**2)
-J0 = 10
+J0 = 100
 Wc = 0.05/dt
 width = 9*dt
 tc = 5*width
@@ -27,41 +27,44 @@ yr=Ny//2
 sigma_max=0.83279*0.45
 kappa_max=3.221*0.95
 
-# solver_Yee=Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True,m=4,sigma_max=sigma_max,kappa_max=kappa_max)
-# solver_Yee.add_source(xs,ys,J0,tc,width,Wc)
-# solver_Yee.add_recorder(xr,yr)
-# # solver_Yee.animate(speed=10)
-# solver_Yee.update_loop()
-# # solver_Yee.show_recorder()
-# solver_Yee.analytical_solution(plot_all=False,frequency_limit=2*Wc)
+solver_Yee=Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True,m=4,sigma_max=sigma_max,kappa_max=kappa_max)
+solver_Yee.add_source(xs,ys,J0,tc,width,Wc)
+solver_Yee.add_recorder(xr,yr)
+# solver_Yee.animate(speed=10)
+
+solver_Yee.update_loop()
+# solver_Yee.show_recorder()
+
+solver_Yee.analytical_solution(plot_all=False,frequency_limit=Wc+3/width)
 
 # Now we do the same for the FCI scheme
 
-# Nx=301
-# Ny=301
-# L = 1
-# dx=np.ones(Nx)*L/Nx
-# dy=np.ones(Ny)*L/Ny
-# Nt=400
-# J0 = 10
-# Wc = 0.05/dt
-# width = 9*dt
-# tc = 5*width
-# tf = 40*tc
-# dt = tf/Nt
+Nx=301
+Ny=301
+L = 1
+dx=np.ones(Nx)*L/Nx
+dy=np.ones(Ny)*L/Ny
+Nt=400
+J0 = 10
+Wc = 0.05/dt
+width = 9*dt
+tc = 5*width
+tf = 40*tc
+dt = tf/Nt
 
-# xs=Nx//2
-# ys=Ny//2
-# xr=3*Nx//5
-# yr=Ny//2
+xs=Nx//2
+ys=Ny//2
+xr=3*Nx//5
+yr=Ny//2
 
-# solver_FCI=FCI(Nt,dx,dy,dt,k_max=1,sigma_max=10,drude=False)
-# solver_FCI.add_source(xs,ys,J0,tc,width,Wc)
-# solver_FCI.add_recorder(xr,yr)
-# solver_FCI.construct_matrices()
-# # solver_FCI.animate(speed=3)
-# solver_FCI.update_loop()
-# solver_FCI.analytical_sol(p_all=False,f_lim=2*Wc)
+solver_FCI=FCI(Nt,dx,dy,dt,k_max=1,sigma_max=10,drude=False)
+solver_FCI.add_source(xs,ys,J0,tc,width,Wc)
+solver_FCI.add_recorder(xr,yr)
+solver_FCI.construct_matrices()
+# solver_FCI.animate(speed=3)
+
+solver_FCI.update_loop()
+solver_FCI.analytical_sol(p_all=False,f_lim=Wc+3/width)
 
 # Now we do the same analysis, but now in the diagonal propagation direction. We again fill in the parameters for Yee
 
--- Yee.py
+++ Yee.py
@@ -103,54 +103,54 @@ solver.restart()
 # Transmission
 ###
 
-n = 2
-eps = n**2
+# n = 2
+# eps = n**2
 
-fig,axes = plt.subplots(4,1,figsize=(6,10))
-i = 0
-colors = ['blue', 'orange', 'green', 'red']
-for d in [30]:
+# fig,axes = plt.subplots(4,1,figsize=(6,10))
+# i = 0
+# colors = ['blue', 'orange', 'green', 'red']
+# for d in [30]:
     
-    delta = c/(2*n*d*L/Nx)
-    print(delta)
+#     delta = c/(2*n*d*L/Nx)
+#     print(delta)
     
-    xs = Nx//4
-    ys = Ny//2
-    xr = 3*Nx//4
-    yr = Ny//2
+#     xs = Nx//4
+#     ys = Ny//2
+#     xr = 3*Nx//4
+#     yr = Ny//2
 
-    solver = Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True)
-    solver.add_source(xs,ys,J0,tc,width,Wc)
-    solver.add_recorder(xr,yr)
-    solver.animate(speed = 10)
-    solver.update_loop()
-    # solver.show_recorder()
+#     solver = Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True)
+#     solver.add_source(xs,ys,J0,tc,width,Wc)
+#     solver.add_recorder(xr,yr)
+#     solver.animate(speed = 10)
+#     solver.update_loop()
+#     # solver.show_recorder()
 
-    padded_Ez_unshielded = np.pad(solver.recorded_Ez, (0, 5*Nt), 'constant')
-    Ez_unshielded = np.fft.rfft(padded_Ez_unshielded)
+#     padded_Ez_unshielded = np.pad(solver.recorded_Ez, (0, 5*Nt), 'constant')
+#     Ez_unshielded = np.fft.rfft(padded_Ez_unshielded)
 
-    solver.restart()
-    solver.add_source(xs,ys,J0,tc,width,Wc)
-    solver.add_recorder(xr,yr)
-    solver.add_material(Nx//2-d//2,Nx//2+d//2,0,Ny,eps_r=eps,mu_r=1,sigma=0)
-    solver.animate(speed = 10)
-    solver.update_loop()
-    # solver.show_recorder()
+#     solver.restart()
+#     solver.add_source(xs,ys,J0,tc,width,Wc)
+#     solver.add_recorder(xr,yr)
+#     solver.add_material(Nx//2-d//2,Nx//2+d//2,0,Ny,eps_r=eps,mu_r=1,sigma=0)
+#     solver.animate(speed = 10)
+#     solver.update_loop()
+#     # solver.show_recorder()
 
-    padded_Ez_shielded = np.pad(solver.recorded_Ez, (0, 5*Nt), 'constant')
-    Ez_shielded = np.fft.rfft(padded_Ez_shielded)
+#     padded_Ez_shielded = np.pad(solver.recorded_Ez, (0, 5*Nt), 'constant')
+#     Ez_shielded = np.fft.rfft(padded_Ez_shielded)
 
-    f=np.fft.rfftfreq(len(padded_Ez_unshielded),dt)
-    # SE=20*np.log10(np.abs(Ez_unshielded/Ez_shielded))
-    T = np.abs(Ez_shielded/Ez_unshielded)**2
-    axes[i].plot(f,T,color=colors[i],label=f'd={d/Nx*L*1e3:.0f} mm')
-    axes[i].legend()
-    axes[i].set_ylabel(r'Transmission')
-    axes[i].set_xlim(Wc/2/np.pi-3/2/np.pi/width,Wc/2/np.pi+3/2/np.pi/width)
-    axes[i].set_ylim(0,5)
-    i+=1
+#     f=np.fft.rfftfreq(len(padded_Ez_unshielded),dt)
+#     # SE=20*np.log10(np.abs(Ez_unshielded/Ez_shielded))
+#     T = np.abs(Ez_shielded/Ez_unshielded)**2
+#     axes[i].plot(f,T,color=colors[i],label=f'd={d/Nx*L*1e3:.0f} mm')
+#     axes[i].legend()
+#     axes[i].set_ylabel(r'Transmission')
+#     axes[i].set_xlim(Wc/2/np.pi-3/2/np.pi/width,Wc/2/np.pi+3/2/np.pi/width)
+#     axes[i].set_ylim(0,5)
+#     i+=1
     
-axes[0].set_title(r'Transmission')
-axes[3].set_xlabel(r'Frequency f [Hz]')
-plt.tight_layout()
-plt.show()
+# axes[0].set_title(r'Transmission')
+# axes[3].set_xlabel(r'Frequency f [Hz]')
+# plt.tight_layout()
+# plt.show()
