1eaba176c7febd5ec6e89fe1b38005a3abaad674
jarnelonneux <jarne.lonneux@gmail.com>
2026-04-30 11:03:43 +0200

    Drude final

--- drude.py
+++ drude.py
@@ -49,26 +49,7 @@ sigma_DC = 0.1
 gamma = 1/Wc
 print('Gamma: ',gamma)
 
-# First we do the experiment without material, as a reference
-
-### Run Yee
-solver_Yee = Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True,sigma_max=sigma_max,kappa_max=kappa_max)
-solver_Yee.add_source(xs,ys,J0,tc,width,Wc)
-solver_Yee.add_recorder(xr,yr)
-solver_Yee.update_loop()
-
-### Visualise
-# solver_Yee.show_recorder()
-# # solver_Yee.restart()
-# solver_Yee.animate()
-
-### Extract results
-Ez_Yee_0 = solver_Yee.recorded_Ez
-time_Yee_0 = np.arange(solver_Yee.Nt)*solver_Yee.dt
-Ez_freq_Yee_0 = np.fft.rfft(Ez_Yee_0)
-freq_Yee_0 = 2*np.pi*np.fft.rfftfreq(solver_Yee.Nt,solver_Yee.dt)
-
-### Now we add a normal conducting material
+### First we add a normal conducting material
 ### Run Yee
 solver_Yee = Yee(L,Nx,Ny,Nt,dt,N_PML,PML=True,sigma_max=sigma_max,kappa_max=kappa_max)
 solver_Yee.add_source(xs,ys,J0,tc,width,Wc)
@@ -83,9 +64,10 @@ solver_Yee.update_loop()
 
 ### Extract results
 Ez_Yee_normal = solver_Yee.recorded_Ez
-time_Yee_normal = np.arange(solver_Yee.Nt)*solver_Yee.dt
+time_Yee_normal = np.arange(solver_Yee.Nt*6)*solver_Yee.dt
+Ez_Yee_normal = np.pad(Ez_Yee_normal,(0,5*solver_Yee.Nt),'constant')
 Ez_freq_Yee_normal = np.fft.rfft(Ez_Yee_normal)
-freq_Yee_normal = 2*np.pi*np.fft.rfftfreq(solver_Yee.Nt,solver_Yee.dt)
+freq_Yee_normal = np.fft.rfftfreq(6*solver_Yee.Nt,solver_Yee.dt)
 
 ### Now we use gamma so it behaves like a drude material
 
@@ -103,9 +85,10 @@ solver_Yee.update_loop()
 
 ### Extract results
 Ez_Yee_drude = solver_Yee.recorded_Ez
-time_Yee_drude = np.arange(solver_Yee.Nt)*solver_Yee.dt
+time_Yee_drude = np.arange(6*solver_Yee.Nt)*solver_Yee.dt
+Ez_Yee_drude = np.pad(Ez_Yee_drude,(0,5*solver_Yee.Nt),'constant')
 Ez_freq_Yee_drude = np.fft.rfft(Ez_Yee_drude)
-freq_Yee_drude = 2*np.pi*np.fft.rfftfreq(solver_Yee.Nt,solver_Yee.dt)
+freq_Yee_drude = np.fft.rfftfreq(6*solver_Yee.Nt,solver_Yee.dt)
 
 # FCI
 
@@ -125,26 +108,7 @@ dx[x_start:x_einde] = dx[x_start:x_einde]/factor
 k_max=1
 sigma_max=1
 
-### First we analyse without material as a reference
-### Run FCI
-solver_FCI=FCI(Nt,dx,dy,dt,k_max,sigma_max,drude=True)
-solver_FCI.add_source(xs,ys,J0,tc,width,Wc)
-solver_FCI.add_recorder(xr,yr)
-solver_FCI.construct_matrices()
-solver_FCI.update_loop_drude()
-
-### Visualise
-# solver_FCI.show_recorder()
-# # solver_FCI.restart()
-# solver_FCI.animate()
-
-### Extract results
-Ez_FCI_0 = solver_FCI.recorded_Ez
-time_FCI_0 = np.arange(solver_FCI.Nt)*solver_FCI.dt
-Ez_freq_FCI_0 = np.fft.rfft(Ez_FCI_0)
-freq_FCI_0 = 2*np.pi*np.fft.rfftfreq(solver_FCI.Nt,solver_FCI.dt)
-
-### Now we investigate again the normal material
+### First we investigate again the normal material
 ### Run FCI
 solver_FCI=FCI(Nt,dx,dy,dt,k_max,sigma_max,drude=True)
 solver_FCI.add_source(xs,ys,J0,tc,width,Wc)
@@ -160,9 +124,10 @@ solver_FCI.update_loop_drude()
 
 ### Extract results
 Ez_FCI_normal = solver_FCI.recorded_Ez
-time_FCI_normal = np.arange(solver_FCI.Nt)*solver_FCI.dt
+time_FCI_normal = np.arange(6*solver_FCI.Nt)*solver_FCI.dt
+Ez_FCI_normal = np.pad(Ez_FCI_normal,(0,5*solver_FCI.Nt),'constant')
 Ez_freq_FCI_normal = np.fft.rfft(Ez_FCI_normal)
-freq_FCI_normal = 2*np.pi*np.fft.rfftfreq(solver_FCI.Nt,solver_FCI.dt)
+freq_FCI_normal = np.fft.rfftfreq(6*solver_FCI.Nt,solver_FCI.dt)
 
 ### Lastly we again investigate the drude material
 ### Run FCI
@@ -180,9 +145,10 @@ solver_FCI.update_loop_drude()
 
 ### Extract results
 Ez_FCI_drude = solver_FCI.recorded_Ez
-time_FCI_drude = np.arange(solver_FCI.Nt)*solver_FCI.dt
+time_FCI_drude = np.arange(6*solver_FCI.Nt)*solver_FCI.dt
+Ez_FCI_drude = np.pad(Ez_FCI_drude,(0,5*solver_FCI.Nt),'constant')
 Ez_freq_FCI_drude = np.fft.rfft(Ez_FCI_drude)
-freq_FCI_drude = 2*np.pi*np.fft.rfftfreq(solver_FCI.Nt,solver_FCI.dt)
+freq_FCI_drude = np.fft.rfftfreq(6*solver_FCI.Nt,solver_FCI.dt)
 
 ### Plot extracted results
 
@@ -197,37 +163,17 @@ plt.title('Recorded electric field behind a conductor and drude material slab')
 plt.legend()
 plt.show()
 
-### Plot Drude time domain and reference time domain
-plt.plot(time_Yee_drude,Ez_Yee_drude,label='Drude material (Yee)', linestyle = '-')
-plt.plot(time_Yee_0,Ez_Yee_0,label='Reference signal (no conductor)',linestyle='-.')
-plt.plot(time_FCI_drude,Ez_FCI_drude,label='Drude material (FCI)',linestyle='-')
-plt.plot(time_FCI_0,Ez_FCI_0,label='Reference signal (no conductor) (FCI)',linestyle='-.')
-plt.xlabel('Time (s)')
-plt.ylabel('Electric field (V/m)')
-plt.title('Recorded electric field of reference signal and behind Drude material slab')
-plt.legend()
-plt.show()
+band = (Wc + 5/width)/2/np.pi
+mask = freq_Yee_normal < band
 
-### Plot the frequency domain results (normalised to reference)
-plt.plot(freq_Yee_normal*1e-9,np.abs(Ez_freq_Yee_normal/Ez_freq_Yee_0), label = 'Normal conductor (Yee)', linestyle = '--')
-plt.plot(freq_Yee_drude*1e-9,np.abs(Ez_freq_Yee_drude/Ez_freq_Yee_0),label = 'Drude material (Yee)', linestyle = '-')
-plt.plot(freq_FCI_normal*1e-9,np.abs(Ez_freq_FCI_normal/Ez_freq_FCI_0),label = 'Normal conductor (FCI)', linestyle = '--')
-plt.plot(freq_FCI_drude*1e-9,np.abs(Ez_freq_FCI_drude/Ez_freq_FCI_0),label = 'Drude material (FCI)', linestyle = '-')
+### Plot the transfer function
+plt.plot(freq_Yee_drude[mask]*1e-9,np.abs(Ez_freq_Yee_drude/Ez_freq_Yee_normal)[mask],label = 'Drude material (Yee)')
+plt.plot(freq_FCI_drude[mask]*1e-9,np.abs(Ez_freq_FCI_drude/Ez_freq_FCI_normal)[mask],label = 'Drude material (FCI)')
+plt.plot(Wc/2/np.pi*1e-9**np.ones(2),np.linspace(np.min(np.abs(Ez_freq_Yee_drude/Ez_freq_Yee_normal)[mask]),np.max(np.abs(Ez_freq_Yee_drude/Ez_freq_Yee_normal)[mask]),2),label = 'Cut-off frequency $f_c$',linewidth=0.5)
 plt.xscale('log')
-plt.xlabel('Angular frequency (GHz)')
-plt.ylabel('Electric field (V/m)')
-plt.title('Recorded electric field behind a conductor and drude material slab')
-plt.legend()
-plt.show()
-
-### Plot the frequency domain results (NOT normalised)
-plt.plot(freq_Yee_normal*1e-9,np.abs(Ez_freq_Yee_normal), label = 'Normal conductor (Yee)', linestyle = '--')
-plt.plot(freq_Yee_drude*1e-9,np.abs(Ez_freq_Yee_drude),label = 'Drude material (Yee)', linestyle = '-')
-plt.plot(freq_FCI_normal*1e-9,np.abs(Ez_freq_FCI_normal),label = 'Normal conductor (FCI)', linestyle = '--')
-plt.plot(freq_FCI_drude*1e-9,np.abs(Ez_freq_FCI_drude),label = 'Drude material (FCI)', linestyle = '-')
-plt.xscale('log')
-plt.xlabel('Angular frequency (GHz)')
-plt.ylabel('Electric field (V/m)')
-plt.title('Recorded electric field behind a conductor and drude material slab')
+plt.yscale('log')
+plt.xlabel('Frequency (GHz)')
+plt.ylabel('Relative transmission $T_r$')
+plt.grid(True,which='both')
 plt.legend()
 plt.show()
