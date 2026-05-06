Units fixed

Class_RTD.py
@@ -15,7 +15,7 @@ class RTD:
         self.CFL = CFL
         self.a = a
         self.b = b
-        self.Lx = 3*a+2*b+20
+        self.Lx = 3*a+2*b+20*10**(-9)
         self.Ly = Ly
         self.Lz = Lz
         self.t_max = t_max
@@ -50,8 +50,8 @@ class RTD:
     
     def add_barriers(self,U0):
         self.U0 = U0
-        self.U[int((self.a+10)//self.dx):int((self.a+self.b+10)//self.dx)] = U0
-        self.U[int((2*self.a+self.b+10)//self.dx):int((2*self.a+2*self.b+10)//self.dx)] = U0
+        self.U[int((self.a+10*10**(-9))//self.dx):int((self.a+self.b+10*10**(-9))//self.dx)] = U0
+        self.U[int((2*self.a+self.b+10*10**(-9))//self.dx):int((2*self.a+2*self.b+10*10**(-9))//self.dx)] = U0
         self.Kx = np.sqrt(2*self.m*(self.E-U0)/self.hbar**2 + 0j)
         if self.order == 2:
             self.dt = self.CFL*2/(2*hbar.value/(0.023*m_e.value*self.dx**2)+U0/hbar.value)
@@ -59,14 +59,14 @@ class RTD:
             self.dt = self.CFL*2/(8*hbar.value/(3*0.023*m_e.value*self.dx**2)+U0/hbar.value)
         
     def add_potential(self,V0):
-        self.U[int((self.a+10)//self.dx):int((2*self.a+2*self.b+10)//self.dx)] += np.linspace(V0,0,int((self.a+2*self.b)//self.dx+1))
+        self.U[int((self.a+10*10**(-9))//self.dx):int((2*self.a+2*self.b+10*10**(-9))//self.dx)] += np.linspace(V0,0,int((self.a+2*self.b)//self.dx))
         if self.order == 2:
             self.dt = self.CFL*2/(2*hbar.value/(0.023*m_e.value*self.dx**2)+np.max(self.U)/hbar.value)
         elif self.order == 4:
             self.dt = self.CFL*2/(8*hbar.value/(3*0.023*m_e.value*self.dx**2)+np.max(self.U)/hbar.value)
         
     def plot_potential(self):
-        plt.plot(np.arange(self.Nx)*self.dx,self.U/e.value*10**18)
+        plt.plot(np.arange(self.Nx)*self.dx,self.U/e.value)
         plt.xlabel('x')
         plt.ylabel('U')
         plt.xlim(0,self.Lx)
@@ -131,32 +131,33 @@ class RTD:
         fig, axes = plt.subplots(2,1,figsize=(8,6),gridspec_kw={'height_ratios':[3,1]})
         ax = axes[0]
         ax2 = axes[1]
-        im = ax.plot(np.arange(self.Nx)*self.dx,self.psi_Re**2+self.psi_Im**2)[0]
+        im = ax.plot(np.arange(self.Nx)*self.dx*10**9,self.psi_Re**2+self.psi_Im**2)[0]
         ax.set_ylabel(r'$|\psi|^2$')
-        ax.set_xlim(0,self.Lx)
+        ax.set_xlim(0,self.Lx*10**9)
         ax.set_ylim(0,self.C**2)
-        ax.plot(self.xr,self.C**2/10,'ro',label='Recorder')
+        ax.plot(self.xr*10**9,self.C**2/10,'ro',label='Recorder')
         def update(frame):
             for _ in range(speed):
                 self.update()
-            im.set_data(np.arange(self.Nx)*self.dx,self.psi_Re**2+self.psi_Im**2)
+            im.set_data(np.arange(self.Nx)*self.dx*10**9,self.psi_Re**2+self.psi_Im**2)
             return [im]
         
-        ani = FuncAnimation(fig, update, frames=self.Nt//speed, interval=int(self.dt*1000), repeat=repeat)
-        ax2.plot(np.arange(self.Nx)*self.dx,self.U/e.value*10**18)
+        ani = FuncAnimation(fig, update, frames=self.Nt//speed, repeat=repeat)
+        # ani.save("simulation.gif", writer="pillow", fps=10)
+        ax2.plot(np.arange(self.Nx)*self.dx*10**9,self.U/e.value)
         ax2.set_xlabel('x [nm]')
         ax2.set_ylabel('U [eV]')
-        ax2.set_xlim(0,self.Lx)
+        ax2.set_xlim(0,self.Lx*10**9)
         plt.tight_layout()
         ax.legend()
         plt.show()
         
     def show_psi(self):
-        plt.plot(np.arange(self.Nx)*self.dx,self.psi_Re,label='Re')
-        plt.plot(np.arange(self.Nx)*self.dx,self.psi_Im,label='Im')
+        plt.plot(np.arange(self.Nx)*self.dx*10**9,self.psi_Re,label='Re')
+        plt.plot(np.arange(self.Nx)*self.dx*10**9,self.psi_Im,label='Im')
         plt.xlabel('x')
         plt.ylabel(r'$\psi$')
-        plt.xlim(0,self.Lx)
+        plt.xlim(0,self.Lx*10**9)
         plt.legend()
         plt.show()
         

RTD.py
@@ -7,28 +7,28 @@ from Class_RTD import RTD
 from astropy.constants.astropyconst20 import m_e,hbar,e
 import cmath
 
-a = 15
-b = 5
-Lx = 3*a+2*b+20 # Extra space for barrier to not have an influence
-Ly = 40
+a = 15*10**(-9)
+b = 5*10**(-9)
+Lx = (3*a+2*b+20*10**(-9)) # Extra space for barrier to not have an influence
+Ly = 40*10**(-9)
 Lz = Ly
 dx = Lx/3000
-U0 = 0.6*e.value*10**(-18)
-x0 = a/2+5
-sigma_x = a/5
-xr = 7*a/3+2*b+10
+U0 = 0.6*e.value
+x0 = (a/2+5*10**(-9))
+sigma_x = (a/5)
+xr = (7*a/3+2*b+10*10**(-9))
 
-m = 10
-n = 10
+m = 20
+n = 20
 
 m_eff = 0.023*m_e.value
-E = hbar.value**2/(2*m_eff)*((np.pi*n/Ly)**2+(np.pi*m/Lz)**2)*10**18 # in J
+E = hbar.value**2/(2*m_eff)*((np.pi*n/Ly)**2+(np.pi*m/Lz)**2) # in J
 print(f'Energy: {E/e.value} eV')
-kx = np.sqrt(2*m_eff*E/hbar.value**2)*10**(-9) # in 1/nm
-sigma = 5*np.sqrt(E)*10**(-27) # maybe sqrt(E)? idk
-k = 4 # exponent for the absorbing boundary strength
+kx = np.sqrt(2*m_eff*E/hbar.value**2) # in 1/nm
 N_layer = 200
-t_max = 1e-5/np.sqrt(E)
+sigma = np.log(100000)/N_layer/dx*E/kx # maybe sqrt(E)? idk
+k = 4 # exponent for the absorbing boundary strength
+t_max = 1.5*Lx*np.sqrt(m_eff/2/E)
 
 print(f't_max: {t_max}')
 
@@ -46,7 +46,7 @@ print(f't_max: {t_max}')
 
 # solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=2,ABC=True)
 # solver.add_recorder(xr)
-# solver.animate(speed = 1000)
+# solver.animate(speed=1000)
 
 ###
 # With potential barriers:
@@ -113,30 +113,30 @@ print(f't_max: {t_max}')
 # With potential V0
 ###
 
-# V0 = 0.05*e.value/10**18
+V0 = 0.05*e.value
 
-# solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=2,ABC=True)
+solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=2,ABC=True)
 
-# solver.add_barriers(U0)
-# solver.add_potential(V0)
-# solver.plot_potential()
-# solver.add_recorder(xr)
-# solver.animate(speed = 1000)
-# solver.restart()
+solver.add_barriers(U0)
+solver.add_potential(V0)
+solver.plot_potential()
+solver.add_recorder(xr)
+solver.animate(speed = 1000)
+solver.restart()
 
 # Comparison of orders:
 
-solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=2,ABC=True)
-solver.add_barriers(U0)
-solver.add_recorder(xr)
-solver.update_loop()
-t,J = solver.J_time()
-plt.plot(t,J,label='Order 2')
-solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=4,ABC=True)
-solver.add_barriers(U0)
-solver.add_recorder(xr)
-solver.update_loop()
-t,J = solver.J_time()
-plt.plot(t,J,label='Order 4')
-plt.legend()
-plt.show()
+# solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=2,ABC=True)
+# solver.add_barriers(U0)
+# solver.add_recorder(xr)
+# solver.update_loop()
+# t,J = solver.J_time()
+# plt.plot(t,J,label='Order 2')
+# solver = RTD(dx,a,b,Ly,Lz,t_max,x0,sigma_x,kx,sigma,k,N_layer,order=4,ABC=True)
+# solver.add_barriers(U0)
+# solver.add_recorder(xr)
+# solver.update_loop()
+# t,J = solver.J_time()
+# plt.plot(t,J,label='Order 4')
+# plt.legend()
+# plt.show()